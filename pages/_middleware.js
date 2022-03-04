313eac6a857ada73c53acf33cd395fa4c2af8d49
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-04 20:48:56 +0100

    Ajout du middleware qui redirige l'utilisateur qui est deco.

---
+++ pages/_middleware.js
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+    
+    const token = await getToken({req, secret: process.env.SECRET});
+
+    const {pathname} = req.nextUrl;
+
+    // Si le token exist on autorise l'utilisateur à passer.
+    if(pathname.includes('/api/auth') || token) // Si on veut se login ou on est déjà connecté.
+    {
+        return NextResponse.next(); // Fais en sorte qu'il puisse utiliser le reste de l'appli.
+    }
+
+    if(!token && pathname !== "/login") { // Redirige l'utilisateur vers la page de connexion si il ne s'est jamais connecté.
+        const url = req.nextUrl.clone();
+        url.pathname = "/login";
+        return NextResponse.redirect(url);
+    }
+    
+}
