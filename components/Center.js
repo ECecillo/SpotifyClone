d64e32906ebe547423723822ddd30e75b0b0bd38
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-05 11:44:05 +0100

    Ajout du style et responsive pour affichage des musiques.

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import { shuffle } from "lodash"
 import { playlistState, playlistIdState } from '../atoms/playlistAtom';
@@ -46,7 +46,7 @@ export default function Center() {
         // overflow-y-scroll et scrollbar-hide permettent de faire défiler les éléments et de les afficher au fur et à mesure.
         <div className="flex-grow text-white h-screen overflow-y-scroll scrollbar-hide">
             <header className='absolute top-5 right-8'>
-                <div className="flex items-center bg-black space-x-3 text-white opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                <div className="flex items-center bg-black space-x-3 text-white opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 " onClick={signOut}>
                     <img className="rounded-full w-10 h-10" src={session?.user.image} alt="Profile Image" />
                     <h2>{session?.user.name}</h2>
                     <ChevronDownIcon className='h-5 w-5' />
--- components/Song.js
+++ components/Song.js
@@ -1,22 +1,24 @@
 import useSpotify from '../hooks/useSpotify'
+import {millisToMinutesAndSeconds} from "../lib/time"
 
 function Song({ order, track }) {
     const spotifyApi = useSpotify(); // On aura besoin de l'api lorsque l'on voudra jouer de la musique.
+    console.log(track)
     return (
-        <div className='grid grid-cols-2'>
+        <div className='grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer'>
             <div className='flex items-center space-x-4'>
                 <p>{order + 1}</p>
                 <img className='h-10 w-10'
                     src={track?.track.album.images[0].url}
                     alt="Album url" />
                 <div>
-                    <p>{track.track.name}</p>
-                    <p>{track.track.artists[0].name}</p>
+                    <p className='w-36 lg:w-64 text-white truncate'>{track.track.name}</p>
+                    <p className='w-40'>{track.track.artists[0].name}</p>
                 </div>
             </div>
             <div className='flex items-center justify-between ml-auto md:ml-0'>
-                <p className='hidden md:inline'>{track.track.album.name}</p>
-                <p>duration</p>
+                <p className='w-40 hidden md:inline'>{track.track.album.name}</p>
+                <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
             </div>
         </div>
     )
