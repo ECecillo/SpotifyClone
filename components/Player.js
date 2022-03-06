a2616e950cc406e6bc2a160924230f89411802ea
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-06 09:40:57 +0100

    Ajout des fonctionnalités de lecture  dans Player.js

--- components/Player.js
+++ components/Player.js
@@ -1,10 +1,24 @@
 import { useSession } from 'next-auth/react';
-import React, { useEffect, useState } from 'react'
+import {debounce} from 'lodash';
+import React, { useCallback, useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSpotify from '../hooks/useSpotify'
 import { useSongInfo } from '../hooks/useSongInfo'
-import { SwitchHorizontalIcon } from '@heroicons/react/outline';
+import {
+  HeartIcon,
+  VolumeUpIcon as VolumeDownIcon
+} from '@heroicons/react/outline';
+
+import {
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  RewindIcon,
+  VolumeUpIcon,
+  SwitchHorizontalIcon,
+} from '@heroicons/react/solid';
 
 function Player() {
   /* 
@@ -43,12 +57,46 @@ function Player() {
     }
   };
 
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body?.is_playing) {
+        spotifyApi.pause(); // Dit à spotify de mettre en pause la musique.
+        setIsPlaying(false)
+      }
+      else {
+        spotifyApi.play(); // joue la musique.
+        setIsPlaying(true)
+      }
+    })
+  };
+
+
+
   useEffect(() => {
     if (spotifyApi.getAccessToken && !currentTrackId) { // Si le token est toujours d'actu et que l'on a choisi une musique à jouer.
       fetchCurrentSong();
       setVolume(50);
     }
   }, [currentTrackId, spotifyApi, session])
+
+
+
+
+  useEffect(() => {
+    if(volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]) /* Si le volume change */
+
+  /* useCallback : Appelera cette fonction seulement si on  */
+  const debouncedAdjustVolume = useCallback(
+    // Comme un useEffect avec un setInterval.
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {}); // Envoie une requête à l'API.
+    }, 500),
+    []
+  );
+
 
   return (
     <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
@@ -62,10 +110,30 @@ function Player() {
         </div>
       </div>
       {/* Centre */}
-      <div>
-        <SwitchHorizontalIcon className='w-5 h-5' />
+      <div className='flex items-center justify-evenly'>
+        <SwitchHorizontalIcon className='button' /> {/* Ici on a défini une classe réutilisable dans global */}
+        <RewindIcon onClick={() => spotifyApi.skipToPrevious()} className="button" />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className='button h-10 w-10' />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button h-10 w-10" />
+        )}
+        <FastForwardIcon onClick={() => spotifyApi.skipToNext()} className="button" />
+        <ReplyIcon className='button' />
+      </div>
+      {/* Right (volume ... ) */}
+      <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+        <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume - 10)} className="button" />
+        <input
+          className='w-14 md:w-28'
+          type="range"
+          value={volume}
+          min={0}
+          max={100}
+          onChange={e => setVolume(Number(e.target.value))} /> 
+          {/* On va utiliser une debounce function pour éviter de surcharger l'API et limiter les perfs. */}
 
-
+        <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className="button" />
       </div>
     </div>
   )
